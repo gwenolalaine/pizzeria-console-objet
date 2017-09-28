f4ccc9d3221e140056065618616be56d83c9d903
Gwenola LAINE <laineg.contact@gmail.com>
2017-09-28 14:52:25 +0200

    1er maven

---
+++ src/main/java/fr/pizzeria/exception/StockageException.java
@@ -0,0 +1,11 @@
+package fr.pizzeria.exception;
+
+public class StockageException extends Exception{
+	public StockageException(){
+		
+	}
+	
+	public StockageException(String msg){
+		super(msg);
+	}
+}
---
+++ src/main/java/fr/pizzeria/model/ToString.java
@@ -0,0 +1,14 @@
+package fr.pizzeria.model;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention (RetentionPolicy.RUNTIME)
+@Target (ElementType.FIELD)
+public @interface ToString {
+	boolean upperCase = false;
+
+	boolean upperCase();
+}
